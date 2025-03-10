f58280c52554257ad71b0b0272dd4903e5a18a0b
Cillian <cillian.murchu@gmail.com>
2025-03-10 05:40:57 +0100

    simple license plate generator input

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,8 +8,9 @@ import { MatChipsModule } from '@angular/material/chips';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { MatButtonModule } from '@angular/material/button';
 import { FormsModule } from '@angular/forms';
-import { interval, Subscription, timer } from 'rxjs';
-import { MatSliderChange, MatSliderModule } from '@angular/material/slider';
+import { BehaviorSubject, interval, map, Subscription, timer } from 'rxjs';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 interface RequestStatus {
   [url: string]: StatusType;
@@ -39,10 +40,16 @@ type StatusType =
     MatExpansionModule,
     MatButtonModule,
     MatSliderModule,
+    MatFormFieldModule,
   ],
 })
 export class AppComponent {
   requestStatuses: RequestStatus = {}; // Track status of each URL request
+  private inputSubject = new BehaviorSubject<number>(0);
+  licensePlate$ = this.inputSubject
+    .asObservable()
+    .pipe(map((n) => this.generateLicensePlate(n)));
+
   progress: Progress | null = null; // Track progress (0 to 100)
   completedRequests: number = 0;
   urls = TEST_URLS;
@@ -53,7 +60,23 @@ export class AppComponent {
 
   constructor(private dataService: DataService) {}
 
-  ngOnInit() {}
+  onInputChange(event: Event) {
+    const value = (event.target as HTMLInputElement).value;
+    const num = parseInt(value, 10);
+    this.inputSubject.next(isNaN(num) || num < 0 ? 0 : num);
+  }
+
+  private generateLicensePlate(n: number): string {
+    const chars = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    let plate = '';
+
+    for (let i = 0; i < 6; i++) {
+      plate = chars[n % 36] + plate;
+      n = Math.floor(n / 36);
+    }
+
+    return plate;
+  }
 
   setMaxConcurrency(value: number) {
     this.maxConcurrency = value;
@@ -71,6 +94,12 @@ export class AppComponent {
       this.timerSubscription.unsubscribe();
       this.timerSubscription = null;
     }
+  }
+
+  get allCompleted() {
+    console.log('this.urls.length; is ', this.urls.length);
+    console.log('this.completedRequests; is ', this.completedRequests);
+    return this.completedRequests === this.urls.length;
   }
 
   async stopFetching() {
@@ -118,8 +147,8 @@ export class AppComponent {
       this.setRequestStatus(url, 'Failed', 0);
     } else {
       this.setRequestStatus(url, 'Success', 100);
-      this.completedRequests++;
     }
+    this.completedRequests++;
   }
 
   private handleError(error: any) {
